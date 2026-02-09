c772651b2e957057453be5940181b1adc09adf74
charlie1728 <carlosverbel1708@gmail.com>
2026-02-09 13:13:26 +0000

    punto 5 y 6

--- BibliotecaApp.java
+++ BibliotecaApp.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BibliotecaApp {
-
-    // prestamo = [idPrestamo, nombreUsuario, tituloLibro, diasPrestamo, multaPorDia]
     static ArrayList<ArrayList<Object>> prestamos = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
@@ -15,12 +13,19 @@ public class BibliotecaApp {
 
             switch (opcion) {
                 case 1: registrarPrestamo();
+                break;
                 case 2: mostrarPrestamos();
+                break;
                 case 3: buscarPrestamoPorId();
+                break;
                 case 4: actualizarPrestamo();
+                break;
                 case 5: eliminarPrestamo();
+                break;
                 case 6: calcularTotalMultas();
+                break;
                 case 7: System.out.println("Saliendo...");
+                break;
                 default: System.out.println("Opción inválida.");
             }
             System.out.println();
@@ -40,50 +45,63 @@ public class BibliotecaApp {
         System.out.println("7. Salir");
     }
 
-    // ====== CRUD (por implementar) ======
-    static void registrarPrestamo() { /* TODO */ }
-    static void mostrarPrestamos() { /* TODO */ }
-    static void buscarPrestamoPorId() { /* TODO */ }
-    static void actualizarPrestamo() { /* TODO */ }
-    static void eliminarPrestamo() { 
-        
-        System.out.println("Ingrese el ID del préstamo a eliminar: ")
-        int idPrestamo=sc.nextInt();
+    static void registrarPrestamo() { System.out.println("(Función registrarPrestamo pendiente de copiar del código 1)"); }
+    static void mostrarPrestamos() { System.out.println("(Función mostrarPrestamos pendiente de copiar del código 1)"); }
+    static void buscarPrestamoPorId() { System.out.println("(Función buscarPrestamoPorId pendiente de copiar del código 2)"); }
+    static void actualizarPrestamo() { System.out.println("(Función actualizarPrestamo pendiente de copiar del código 2)"); }
+    static void eliminarPrestamo() {
+        System.out.println("--- Eliminar Préstamo ---");
+        int idBuscado = leerEntero("Ingrese el ID del préstamo a eliminar: ");
         boolean eliminado = false;
 
-for (int i = 0; i < prestamos.size(); i++) {
-        if (prestamos.get(i).getIdPrestamo() == idBuscado) {
-            prestamos.remove(i);
-            eliminado = true;
-            System.out.println("Préstamo eliminado correctamente");
-            break;
+        for (int i = 0; i < prestamos.size(); i++) {
+            int idActual = (int) prestamos.get(i).get(0);
+            
+            if (idActual == idBuscado) {
+                prestamos.remove(i);
+                eliminado = true;
+                System.out.println("Préstamo eliminado correctamente.");
+                break;
+            }
+        }
+
+        if (!eliminado) {
+            System.out.println("No existe un préstamo con ese ID.");
         }
     }
 
-    if (!eliminado) {
-        System.out.println("No existe un préstamo con ese ID");
-    }
+    static void calcularTotalMultas() {
+        System.out.println("--- Calcular Multa ---");
+        int idBuscado = leerEntero("Ingrese el ID del préstamo a evaluar: ");
+        boolean encontrado = false;
 
+        for (ArrayList<Object> p : prestamos) {
+            int idActual = (int) p.get(0);
+            
+            if (idActual == idBuscado) {
+                encontrado = true;
+                int diasPactados = (int) p.get(3);
+                int multaPorDia = (int) p.get(4);
 
-     }
+                System.out.println("Días pactados originalmente: " + diasPactados);
+                int diasUsados = leerEntero("Digite los días que usó el libro: ");
 
-
-    // ====== Cálculo (por implementar) ======
-    static void calcularTotalMultas() { 
-        int diasUsados;
-        System.out.println("Digite los dias que uso el libro");
-        diasUsados=sc.nextInt;
-
-        if(diasUsados>diasPrestamo){
-        multas=(diasUsados-diasPrestamos)*multaPorDia 
-        System.out.println("El total de la multa es: "+multas);
-        } else{
-            System.out.println("No tiene multa");
+                if (diasUsados > diasPactados) {
+                    int diasRetraso = diasUsados - diasPactados;
+                    int totalMulta = diasRetraso * multaPorDia;
+                    System.out.println("¡Retraso de " + diasRetraso + " días!");
+                    System.out.println("El total de la multa es: " + totalMulta);
+                } else {
+                    System.out.println("Entrega a tiempo. No tiene multa.");
+                }
+                break;
+            }
+        }
+        
+        if (!encontrado) {
+            System.out.println("No se encontró el préstamo con ese ID.");
         }
     }
-    
-
-    // ====== Utilidades mínimas ======
     static int leerEntero(String msg) {
         while (true) {
             System.out.print(msg);
